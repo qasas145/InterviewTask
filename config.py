f901663f49e5944e89420ec61f8b255978ec7099
qasas145 <mohamed.sayed14527@yahoo.com>
2024-10-26 20:29:43 +0300

    Main thread app

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your_jwt_secret_key")
+    SECRET_KEY = os.getenv("SECRET_KEY", "muhammad_elsayed")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "muhammad_elqasas")
     MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongodb:27017/mydatabase")
